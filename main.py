3b801ea0c6b11d1410af9c1eb8cd84f110ae3259
Aleksandar Suka <suka61s@hotmail.com>
2017-06-27 20:16:11 +0200

    Implementing stohastic gradient respect to batch size.

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import numpy as np
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 net = network.Network([784, 30, 10])
-net.fit(training_data, validation_data, epochs = 30, alpha = 3.0)
+net.fit(training_data, validation_data, epochs = 30, alpha = 3.0, batch_size = 10)
 #net.SGD()
--- network.py
+++ network.py
@@ -7,15 +7,20 @@ class Network:
 		self.weights = np.array([ np.random.randn(y,x) for x,y in zip(layers[:-1],layers[1:])])
 		self.bias = np.array([ np.random.randn(x,1) for x in layers[1:]])
 
-	def fit(self, train_data, validation_data, epochs, alpha, test_data = None):
-
+	def fit(self, train_data, validation_data, epochs = 1, alpha = 0.1, test_data = None, batch_size = None):
 
 		print("Epoch: -1 of",epochs,"- Score: ",self.evaluate(validation_data)," / ",len(validation_data))
 		for i in range(epochs):
-			self.update_network(train_data, alpha)
+			if batch_size is None:
+				self.update_network(train_data, alpha)
+			else:
+				batches = np.split(train_data, len(train_data)/batch_size)
+				for batch in batches:
+					self.update_network(batch, alpha)
 			print("Epoch: ",i," of",epochs,"- Score: ",self.evaluate(validation_data)," / ",len(validation_data))
 
 	def update_network(self, train_data, alpha):
+
 		weights_grad_total = [0 for x in range(self.num_layers-1)]
 		bias_grad_total = [0 for x in range(self.num_layers-1)]
 		for x,y in train_data:
@@ -45,6 +50,7 @@ class Network:
 			bias_grad[-i-1] = delta
 			weights_grad[-i-1] = np.dot(delta, activations[-i-2].T)
 		return bias_grad, weights_grad
+		
 	def evaluate(self,validation_data):
 		score = 0
 		for x,y in validation_data:
@@ -52,6 +58,8 @@ class Network:
 			for i in range(self.num_layers-1):
 				activations = sigmoid(np.dot(self.weights[i], activations)+self.bias[i])
 			output = np.argmax(activations)
+			if(isinstance(y, (np.ndarray))):
+				y = np.argmax(y)
 			if(output == y):
 				score+=1
 		return score
